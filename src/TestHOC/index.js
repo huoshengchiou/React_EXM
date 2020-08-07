5de6b3a76e4ee4917041f8c85d25fc902c11f4f1
Chiou <Sheng.Chiou@aceraeb.com>
2020-08-07 18:08:30 +0800

    update

--- src/TestHOC/index.js
+++ src/TestHOC/index.js
@@ -4,12 +4,12 @@ import React from 'react'
 
 const HOC = (Component1, Component2) => ({ name = 'sheng' }) => {
 
-
-
+    //   設計原則遵循functional programming (FP)  pure no sideeffect
+    // 假設當有多個 component 都要做 say good morning 這件事，只要都掛入 HOC 即可，不用在每個元件都重寫 say 的代碼
     return (
         <>
-            {Component1 && <Component1 name='sheng' />}
-            {Component2 && <Component2 name='sheng' />}
+            {Component1 && <Component1 name={name} />}
+            {Component2 && <Component2 name={name} />}
         </>
     )
 
